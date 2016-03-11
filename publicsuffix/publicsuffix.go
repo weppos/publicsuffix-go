54f05ba003a52019035e790333308396936dee6b
Simone Carletti <weppos@weppos.net>
2016-03-11 22:57:35 +0100

    Define rule struct and types

--- publicsuffix/publicsuffix.go
+++ publicsuffix/publicsuffix.go
@@ -1,3 +1,33 @@
 package publicsuffix
 
+import ()
 
+const (
+	NormalType    = 1
+	WildcardType  = 2
+	ExceptionType = 3
+)
+
+type Rule struct {
+	Type    int
+	Value   string
+	Private bool
+}
+
+func NewRule(content string) *Rule {
+	var rtype int
+	var value string
+
+	switch content[0:1] {
+	case "*":
+		rtype = WildcardType
+		value = content[2:len(content)]
+	case "!":
+		rtype = ExceptionType
+		value = content[1:len(content)]
+	default:
+		rtype = NormalType
+		value = content
+	}
+	return &Rule{Type: rtype, Value: value}
+}
--- publicsuffix/publicsuffix_test.go
+++ publicsuffix/publicsuffix_test.go
@@ -1,1 +1,33 @@
 package publicsuffix
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewRule_Normal(t *testing.T) {
+	rule := NewRule("com")
+	want := &Rule{Type: NormalType, Value: "com"}
+
+	if !reflect.DeepEqual(want, rule) {
+		t.Errorf("NewRule returned %v, want %v", rule, want)
+	}
+}
+
+func TestNewRule_Wildcard(t *testing.T) {
+	rule := NewRule("*.example.com")
+	want := &Rule{Type: WildcardType, Value: "example.com"}
+
+	if !reflect.DeepEqual(want, rule) {
+		t.Errorf("NewRule returned %v, want %v", rule, want)
+	}
+}
+
+func TestNewRule_Exception(t *testing.T) {
+	rule := NewRule("!example.com")
+	want := &Rule{Type: ExceptionType, Value: "example.com"}
+
+	if !reflect.DeepEqual(want, rule) {
+		t.Errorf("NewRule returned %v, want %v", rule, want)
+	}
+}
