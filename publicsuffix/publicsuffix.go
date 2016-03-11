01c7d80f8467d60202ba70465976ee37cc53c266
Simone Carletti <weppos@weppos.net>
2016-03-11 23:25:25 +0100

    Compute Rule length when the rule is created
    The length is used when selecting the most relevant rule for a name.
    The longest rule wins.

--- publicsuffix/publicsuffix.go
+++ publicsuffix/publicsuffix.go
@@ -1,6 +1,8 @@
 package publicsuffix
 
-import ()
+import (
+	"strings"
+)
 
 const (
 	NormalType    = 1
@@ -11,23 +13,28 @@ const (
 type Rule struct {
 	Type    int
 	Value   string
+	Length  int
 	Private bool
 }
 
 func NewRule(content string) *Rule {
-	var rtype int
+	var rule *Rule
 	var value string
 
 	switch content[0:1] {
 	case "*":
-		rtype = WildcardType
 		value = content[2:len(content)]
+		rule = &Rule{Type: WildcardType, Value: value, Length: len(Labels(value)) + 1}
 	case "!":
-		rtype = ExceptionType
 		value = content[1:len(content)]
+		rule = &Rule{Type: ExceptionType, Value: value, Length: len(Labels(value))}
 	default:
-		rtype = NormalType
 		value = content
+		rule = &Rule{Type: NormalType, Value: value, Length: len(Labels(value))}
 	}
-	return &Rule{Type: rtype, Value: value}
+	return rule
+}
+
+func Labels(name string) []string {
+	return strings.Split(name, ".")
 }
--- publicsuffix/publicsuffix_test.go
+++ publicsuffix/publicsuffix_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestNewRule_Normal(t *testing.T) {
 	rule := NewRule("com")
-	want := &Rule{Type: NormalType, Value: "com"}
+	want := &Rule{Type: NormalType, Value: "com", Length: 1}
 
 	if !reflect.DeepEqual(want, rule) {
 		t.Errorf("NewRule returned %v, want %v", rule, want)
@@ -16,7 +16,7 @@ func TestNewRule_Normal(t *testing.T) {
 
 func TestNewRule_Wildcard(t *testing.T) {
 	rule := NewRule("*.example.com")
-	want := &Rule{Type: WildcardType, Value: "example.com"}
+	want := &Rule{Type: WildcardType, Value: "example.com", Length: 3}
 
 	if !reflect.DeepEqual(want, rule) {
 		t.Errorf("NewRule returned %v, want %v", rule, want)
@@ -25,9 +25,23 @@ func TestNewRule_Wildcard(t *testing.T) {
 
 func TestNewRule_Exception(t *testing.T) {
 	rule := NewRule("!example.com")
-	want := &Rule{Type: ExceptionType, Value: "example.com"}
+	want := &Rule{Type: ExceptionType, Value: "example.com", Length: 2}
 
 	if !reflect.DeepEqual(want, rule) {
 		t.Errorf("NewRule returned %v, want %v", rule, want)
+	}
+}
+
+func TestLabels(t *testing.T) {
+	testCases := map[string][]string {
+		"com": []string{"com"},
+		"example.com": []string{"example", "com"},
+		"www.example.com": []string{"www", "example", "com"},
+	}
+
+	for input, expected := range(testCases) {
+		if output := Labels(input); !reflect.DeepEqual(output, expected) {
+			t.Errorf("Labels(%v) = %v, want %v", input, output, expected)
+		}
 	}
 }
