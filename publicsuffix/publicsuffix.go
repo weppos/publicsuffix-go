217266e6ddf3ac833a2e154556869c3668e26f77
Simone Carletti <weppos@weppos.net>
2016-03-12 13:44:57 +0100

    Add Rule logic to match and decompose a name

--- publicsuffix/publicsuffix.go
+++ publicsuffix/publicsuffix.go
@@ -2,6 +2,8 @@ package publicsuffix
 
 import (
 	"strings"
+	"regexp"
+	"fmt"
 )
 
 const (
@@ -17,23 +19,83 @@ type Rule struct {
 	Private bool
 }
 
+// NewRule parses the rule content, creates and returns a Rule.
 func NewRule(content string) *Rule {
 	var rule *Rule
 	var value string
 
 	switch content[0:1] {
-	case "*":
+	case "*": // wildcard
 		value = content[2:len(content)]
 		rule = &Rule{Type: WildcardType, Value: value, Length: len(Labels(value)) + 1}
-	case "!":
+	case "!": // exception
 		value = content[1:len(content)]
 		rule = &Rule{Type: ExceptionType, Value: value, Length: len(Labels(value))}
-	default:
+	default:  // normal
 		value = content
 		rule = &Rule{Type: NormalType, Value: value, Length: len(Labels(value))}
 	}
 	return rule
 }
+
+// Match checks if the rule matches the name.
+//
+// A domain name is said to match a rule if and only if all of the following conditions are met:
+// - When the domain and rule are split into corresponding labels,
+//   that the domain contains as many or more labels than the rule.
+// - Beginning with the right-most labels of both the domain and the rule,
+//   and continuing for all labels in the rule, one finds that for every pair,
+//   either they are identical, or that the label from the rule is "*".
+//
+// See https://publicsuffix.org/list/
+func (r *Rule) Match(name string) bool {
+	left := strings.TrimSuffix(name, r.Value)
+
+	// the name contains as many labels than the rule
+	// this is a match, unless it's a wildcard
+	// because the wildcard requires one more label
+	if left == "" {
+		return r.Type != WildcardType
+	}
+
+	// if there is one more label, the rule match
+	// because either the rule is shorter than the domain
+	// or the rule is a wildcard and there is one more label
+	return left[len(left)-1:] == "."
+}
+
+// Decompose takes a name as input and decomposes it into a tuple of <TRD+SLD, TLD>,
+// according to the rule definition and type.
+//
+// For exa
+//
+func (r *Rule) Decompose(name string) [2]string {
+	var re *regexp.Regexp
+	suffix := strings.Join(r.parts(), "./")
+
+	switch r.Type {
+	case WildcardType:
+		re = regexp.MustCompile(fmt.Sprintf(`^(.+)\.(.*?\.%s)$`, suffix))
+	default:
+		re = regexp.MustCompile(fmt.Sprintf(`^(.+)\.(%s)$`, suffix))
+	}
+
+	matches := re.FindStringSubmatch(name)
+	if len(matches) < 3 {
+		return [2]string{"", ""}
+	}
+
+	return [2]string{matches[1], matches[2]}
+}
+
+func (r *Rule) parts() []string {
+	labels := Labels(r.Value)
+	if r.Type == ExceptionType {
+		return labels[1:]
+	}
+	return labels
+}
+
 
 func Labels(name string) []string {
 	return strings.Split(name, ".")
--- publicsuffix/publicsuffix_test.go
+++ publicsuffix/publicsuffix_test.go
@@ -32,14 +32,90 @@ func TestNewRule_Exception(t *testing.T) {
 	}
 }
 
+type matchTestCase struct {
+	rule     *Rule
+	input    string
+	expected bool
+}
+
+func TestMatch(t *testing.T) {
+	testCases := []matchTestCase{
+		// standard match
+		matchTestCase{NewRule("uk"), "uk", true},
+		matchTestCase{NewRule("uk"), "example.uk", true},
+		matchTestCase{NewRule("uk"), "example.co.uk", true},
+		matchTestCase{NewRule("co.uk"), "example.co.uk", true},
+
+		// special rules match
+		matchTestCase{NewRule("*.com"), "com", false},
+		matchTestCase{NewRule("*.com"), "example.com", true},
+		matchTestCase{NewRule("*.com"), "foo.example.com", true},
+		matchTestCase{NewRule("!example.com"), "com", false},
+		matchTestCase{NewRule("!example.com"), "example.com", true},
+		matchTestCase{NewRule("!example.com"), "foo.example.com", true},
+
+		// TLD mismatch
+		matchTestCase{NewRule("gk"), "example.uk", false},
+		matchTestCase{NewRule("gk"), "example.co.uk", false},
+
+		// general mismatch
+		matchTestCase{NewRule("uk.co"), "example.co.uk", false},
+		matchTestCase{NewRule("go.uk"), "example.co.uk", false},
+		// rule is longer than input, should not match
+		matchTestCase{NewRule("co.uk"), "uk", false},
+
+		// partial matches/mismatches
+		matchTestCase{NewRule("co"), "example.co.uk", false},
+		matchTestCase{NewRule("example"), "example.uk", false},
+		matchTestCase{NewRule("le.it"), "example.it", false},
+		matchTestCase{NewRule("le.it"), "le.it", true},
+		matchTestCase{NewRule("le.it"), "foo.le.it", true},
+	}
+
+	for _, testCase := range testCases {
+		if testCase.rule.Match(testCase.input) != testCase.expected {
+			t.Errorf("Expected %v to %v match %v", testCase.rule.Value, testCase.expected, testCase.input)
+		}
+	}
+}
+
+type decomposeTestCase struct {
+	rule     *Rule
+	input    string
+	expected [2]string
+}
+
+func TestDecompose(t *testing.T) {
+	testCases := []decomposeTestCase{
+		decomposeTestCase{NewRule("com"), "com", [2]string{"", ""}},
+		decomposeTestCase{NewRule("com"), "example.com", [2]string{"example", "com"}},
+		decomposeTestCase{NewRule("com"), "foo.example.com", [2]string{"foo.example", "com"}},
+
+		decomposeTestCase{NewRule("!british-library.uk"), "uk", [2]string{"", ""}},
+		decomposeTestCase{NewRule("!british-library.uk"), "british-library.uk", [2]string{"british-library", "uk"}},
+		decomposeTestCase{NewRule("!british-library.uk"), "foo.british-library.uk", [2]string{"foo.british-library", "uk"}},
+
+		decomposeTestCase{NewRule("*.com"), "com", [2]string{"", ""}},
+		decomposeTestCase{NewRule("*.com"), "example.com", [2]string{"", ""}},
+		decomposeTestCase{NewRule("*.com"), "foo.example.com", [2]string{"foo", "example.com"}},
+		decomposeTestCase{NewRule("*.com"), "bar.foo.example.com", [2]string{"bar.foo", "example.com"}},
+	}
+
+	for _, testCase := range testCases {
+		if got := testCase.rule.Decompose(testCase.input); !reflect.DeepEqual(got, testCase.expected) {
+			t.Errorf("Expected %v to decompose %v into %v, got %v", testCase.rule.Value, testCase.input, testCase.expected, got)
+		}
+	}
+}
+
 func TestLabels(t *testing.T) {
-	testCases := map[string][]string {
-		"com": []string{"com"},
-		"example.com": []string{"example", "com"},
+	testCases := map[string][]string{
+		"com":             []string{"com"},
+		"example.com":     []string{"example", "com"},
 		"www.example.com": []string{"www", "example", "com"},
 	}
 
-	for input, expected := range(testCases) {
+	for input, expected := range testCases {
 		if output := Labels(input); !reflect.DeepEqual(output, expected) {
 			t.Errorf("Labels(%v) = %v, want %v", input, output, expected)
 		}
