cea16ef440e23a5debf252143bf06636dc9c5ef3
Simone Carletti <weppos@weppos.net>
2019-06-07 10:03:24 +0200

    Test the build

--- test.sh
+++ test.sh
@@ -5,7 +5,8 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     go test -v -race -coverprofile=profile.out -covermode=atomic $d
-    if [[ "$NOCONTEXT" == "1" ]] && [[ $d =~ "generator" ]]; then
+    echo $d
+    if [[ "$NOCONTEXT" == "1" ]] && [[ "$d" =~ "generator" ]]; then
         continue
     fi
     if [ -f profile.out ]; then
