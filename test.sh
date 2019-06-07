87dfa9df8694b2bb6970d855919709bfa086db4e
Simone Carletti <weppos@weppos.net>
2019-06-07 09:49:13 +0200

    Exclude the generator from Go without context

--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
     go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    if [[ "$NOCONTEXT" == "1" ]] && [[ d =~ "generator" ]]; then
+        continue
+    fi
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
