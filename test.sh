e3fe8efd5a0458a073a93e0f58f565be972a6e62
Simone Carletti <weppos@weppos.net>
2019-08-08 09:49:29 +0200

    Require Go 1.8

--- test.sh
+++ test.sh
@@ -4,10 +4,7 @@ set -e
 echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    if [[ "$NOCONTEXT" == "1" ]] && [[ "$d" =~ "generator" ]]; then
-        continue
-    fi
-    go test -v -race -coverprofile=profile.out -covermode=atomic $d
+    go test -v -race -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
